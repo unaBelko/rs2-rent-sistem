440a6e524da1205bd934d0c69f860d219f825644
unaBelko <una.belko@edu.fit.ba>
2024-07-06 23:22:06 +0200

    Initial commit - created flutter app

---
+++ rs2_rent_sistem/lib/models/temp.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Temp extends StatelessWidget {
+  const Temp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
